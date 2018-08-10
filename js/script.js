cd82566c91f64e6dcd32f9693b97e0d99600a617
Olga Bezginova <bezginova.olga@gmail.com>
2018-08-10 21:30:31 +0300

    Initialize table for new game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/*function randomNumber(min, max) {
+function randomNumber(min, max) {
   return min + Math.floor(Math.random() * (max + 1 - min));
-}*/
+}
 
 const tableSize = 4;
 
@@ -23,3 +23,33 @@ for(let i = 0; i < tableSize; i++){
 
 table.appendChild(tbody);
 document.body.appendChild(table);
+
+//Create number 2 or 4 in a random cell
+function newNumber(){    
+    let emptyCells = document.querySelectorAll('.empty');
+    let randCell = emptyCells[randomNumber(0, emptyCells.length-1)];
+    randCell.textContent = randomNumber(1,2)*2;
+    randCell.classList.remove('empty');
+    return randCell;
+}
+
+//Initialize table for new game
+function initTable(){
+    for (var i = 0; i < tableSize; i++) {
+        for (var j = 0; j < tableSize; j++) {
+            table.rows[i].cells[j].className = 'empty';
+            table.rows[i].cells[j].textContent = '';            
+        }
+    }
+    let twoUniqueCells = false;
+    while(!twoUniqueCells){ 
+        let initNumber0 = newNumber();
+        let initNumber1 = newNumber();
+        if(initNumber0 !== initNumber1){
+            twoUniqueCells = true;   
+        }        
+    } 
+}
+
+
+
